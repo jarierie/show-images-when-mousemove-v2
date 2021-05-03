ee89beb068600c41f89aed7bef05fd4287d146a4
Jarie <jarielleichiban@gmail.com>
2021-05-03 14:09:09 +0800

    show and fade elements with mouse move v2

---
+++ src/components/Card.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { gsap, Linear } from "gsap";
+const CardContainer = styled.div`
+  top: 0;
+  left: 0;
+  width: 300px;
+  height: 400px;
+  background-color: ${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  visibility: hidden;
+  overflow: hidden;
+`;
+
+const Card = (props) => {
+  const ref = useRef(null);
+  const { img, currentItem, index, position } = props;
+
+  //animation
+  const animation = () => {
+    const tl = gsap.timeline();
+    tl.to(ref.current, {
+      x: position.x,
+      y: position.y,
+      autoAlpha: 1,
+      duration: 0.5,
+      delay: index * 0.3,
+      ease: Linear.easeIn,
+    }).to(
+      ref.current,
+      {
+        visibility: "hidden",
+
+        delay: 1,
+        duration: 0,
+      },
+      1
+    );
+  };
+
+  useEffect(() => {
+    if (index === currentItem) {
+      animation();
+    } else {
+      return;
+    }
+  });
+  return (
+    <>
+      <CardContainer ref={ref}>
+        <img src={img} alt=''></img>
+      </CardContainer>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/components/Main.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { gsap } from "gsap";
+import meenoi1 from "../assets/meenoi.jpg";
+import meenoi2 from "../assets/meenoi2.jpg";
+import meenoi3 from "../assets/meenoi3.jpg";
+import meenoi4 from "../assets/meenoi4.jpg";
+import meenoi5 from "../assets/meenoi5.jpg";
+import Card from "./Card";
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+  min-height: 100vh;
+  background-color: #eeeeee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const TextContainer = styled.div`
+  width: 900px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 100px;
+  font-weight: bolder;
+  letter-spacing: -5px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Main = () => {
+  const array = [meenoi1, meenoi2, meenoi3, meenoi4, meenoi5];
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [currentItem, setCurrentItem] = useState(0);
+
+  useEffect(() => {
+    const set = (e) => {
+      setPosition({ x: e.pageX, y: e.pageY });
+    };
+    window.addEventListener("mousemove", set);
+    return () => {
+      window.removeEventListener("mousemove", set);
+    };
+  }, []);
+
+  const hoverSet = () => {
+    if (currentItem === array.length - 1) {
+      setCurrentItem(0);
+    } else {
+      setCurrentItem(currentItem + 1);
+    }
+  };
+  return (
+    <>
+      <Container onMouseMove={hoverSet}>
+        <TextContainer>Hover over me</TextContainer>
+        {array.map((item, index) => {
+          return (
+            <>
+              <Card
+                position={position}
+                index={index}
+                currentItem={currentItem}
+                img={item}
+              ></Card>
+            </>
+          );
+        })}
+        <h1>{currentItem}</h1>
+      </Container>{" "}
+      <Container></Container>
+    </>
+  );
+};
+
+export default Main;
